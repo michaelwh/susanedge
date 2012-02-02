6517dde45adb10883909be2b86adf052eca307ac
Michael Hodgson <michaelwhodgson@gmail.com>
2012-02-02 14:14:45 +0000

    Added simple susan edge detection code with square mask and pixel loop

--- susanedge/susanedge.py
+++ susanedge/susanedge.py
@@ -4,15 +4,30 @@ import scipy.ndimage
 import numpy as np
 
 
+
+def simple_susan(img, thresh=27.0):
+    area = np.zeros_like(img)
+    for x in range(1, len(img) - 1):
+        print x
+        for y in range(1, len(img[x]) - 1):
+            area[x, y] = 0.0
+            for x1 in range(x-1, x+2):
+                for y1 in range(y-1, y+2):
+                    if abs(img[x1, y1] - img[x, y]) < thresh:
+                        area[x, y] += 1
+    return area
+        
+
+
 if __name__ == '__main__':
-    imgin = Image.open("/home/mh23g08/susanedge/susanedge/test_data/fish_image.jpg")
+    imgin = Image.open("/home/mh23g08/susanedge/susanedge/test_data/simple_shapes.png")
     imgin = imgin.convert("L") # convert to greyscale (luminance)
     
     img = np.asarray(imgin)
     img = img.astype(np.float32) # convert to a floating point
     
-    s = scipy.ndimage.sobel(img)
+    area_img = simple_susan(img)   
     
     plt.set_cmap(plt.cm.gray)
-    plt.imshow(s)
+    plt.imshow(area_img)
     plt.show()
